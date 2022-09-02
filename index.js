49f65ed679f8a426d54bde20588b8432f2e75d1d
enjeytee <enjeytee@gmail.com>
2022-09-02 18:09:37 +0800

    af

---
+++ index.js
@@ -0,0 +1,9 @@
+ReactDOM.render(<div>
+    <h1>Hello, everyone!</h1>
+        <ul>
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+        </ul>
+    </div>, 
+    document.querySelector("#root"))
